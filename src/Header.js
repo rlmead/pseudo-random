ffde4134a8769d33327b2da39be092ede0ec65a3
Rebecca Mead <beckyleemead@gmail.com>
2020-10-21 14:49:55 -0400

    closes #3

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Jumbotron, Container, Nav, NavItem, NavLink } from 'reactstrap';
 
 function Header(props) {
     return (
         <>
             {/* static header with restaurant name */}
             {/* exciting food picture (semi-transparent?) as background */}
-            <h1>pseudo random</h1>
-            <p>three forty eight east main street lexington kentucky united states of america</p>
+            {/* https://images.unsplash.com/photo-1561043433-aaf687c4cf04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80 */}
+            {/* {
+                let imgStyle = {
+                backgroundImage: 'url(https://images.unsplash.com/photo-1561043433-aaf687c4cf04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)'
+            };
+            } */}
+
+            <Jumbotron
+                fluid
+                style={{backgroundImage:'url(https://images.unsplash.com/photo-1544070078-a212eda27b49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80)'}}>
+                <Container
+                className='text-light'>
+                    <h1>pseudo random</h1>
+                    <p>three forty eight east main street lexington kentucky united states of america</p>
+                </Container>
+            </Jumbotron>
+
             {/* nav bar with buttons that run setMenuView */}
             <Nav
-                justified={true}
-                tabs={true}>
+                justified
+                tabs>
                 {
                     props.menuSections.map((item, index) => {
                         return (
